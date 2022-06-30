69fbaff01402ebfcdd685e027389a8fbc0be96ba
Ashley Wong <tofudragon333@gmail.com>
2022-06-30 16:45:41 -0400

    commit tree d4cf8c5d14e037de019ebde0c84d9c8b05deaec6

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+    def match(array)
+        #does something
+        new_array = []
+        array.select do |item|
+        item.chars.sort == word.chars.sort
+        end
+    end
+end
